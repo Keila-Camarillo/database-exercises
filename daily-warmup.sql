5e2ef947acb0fb9038c3e2f4bbf1774dfa84c422
Keila-Camarillo <keila.a.camarillo@gmail.com>
2023-03-10 09:24:30 -0600

    Add daily warm-up

---
+++ daily-warmup.sql
@@ -0,0 +1,13 @@
+-- using the chipotle database,
+-- find how many times someone ordered a chicken or veggie bowl
+-- with a quantity greater than 1
+SHOW databases;
+USE chipotle;
+SHOW tables;
+
+SELECT * 
+FROM orders
+WHERE item_name IN ('chicken bowl', 'veggie bowl')
+	AND quantity > 1;
+
+-- 
